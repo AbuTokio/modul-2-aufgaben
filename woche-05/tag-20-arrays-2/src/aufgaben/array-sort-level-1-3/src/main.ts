c9bdef65f8c480aa7edf03a5974dc8349272a2a9
AbuTokio <bunyaminxkara@gmail.com>
2025-07-31 12:46:58 +0200

    Array Sort - Level 1-3

---
+++ woche-05/tag-20-arrays-2/src/aufgaben/array-sort-level-1-3/src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css"
+
+const numArray1: number[] = [36, 24, 22, 3, 2, 98, 88, 99, 10, 54, 68, 70]
+console.log(numArray1)
+
+numArray1.sort((a, b) => a - b)
+console.log(numArray1)
+
+numArray1.sort((a, b) => b - a)
+console.log(numArray1)
