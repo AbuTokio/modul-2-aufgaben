b27245ba2a79ee2ce9c0091509c48bd8ac437a09
AbuTokio <bunyaminxkara@gmail.com>
2025-08-01 15:50:20 +0200

    Loops - Level 2-4

---
+++ woche-05/tag-21-loops/src/aufgaben/loops-level-2-4/src/main.ts
@@ -0,0 +1,14 @@
+import "./style.css"
+
+function addToFifty(): void {
+  const limit = 50
+  let counter = 0
+
+  while (counter < limit) {
+    counter += Number((Math.random() * 10).toFixed())
+  }
+
+  console.log(counter + "> 50. Stopping right here.")
+}
+
+addToFifty()
