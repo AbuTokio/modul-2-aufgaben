602e1b5663ef37358ca810963b7f3ca1f8065bab
AbuTokio <bunyaminxkara@gmail.com>
2025-08-13 12:38:39 +0200

    BOM - Level 1-2

---
+++ src/aufgaben/bom/bom-level-1-2/main.ts
@@ -0,0 +1,19 @@
+import "../../../style.css"
+import "./style.css"
+
+const htmlElements = {
+  timeDiv: document.querySelector(".zeit") as HTMLDivElement,
+  startButton: document.querySelector("#btn") as HTMLButtonElement,
+}
+
+htmlElements.startButton.addEventListener("click", () => {
+  let zeit: number = 100
+
+  const timer = setInterval(() => {
+    if (zeit <= 0) {
+      clearInterval(timer)
+    }
+    htmlElements.timeDiv.textContent = zeit + "%"
+    zeit--
+  }, 100)
+})
